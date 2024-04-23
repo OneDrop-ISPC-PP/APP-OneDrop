c3faa3ea992aecdea9b90d7e348e2cc6760dc84c
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2024-04-22 22:51:28 -0300

    initial commit

---
+++ Backend/oneDrop/src/main/java/com/pisis/oneDrop/OneDropApplication.java
@@ -0,0 +1,13 @@
+package com.pisis.oneDrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDropApplication.class, args);
+	}
+
+}
---
+++ Backend/oneDrop/src/test/java/com/pisis/oneDrop/OneDropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pisis.oneDrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
