1a57d2f37bd0485830721f96d95cc5267be7f75a
maxicovaro <maxicovaro@gmail.com>
2024-05-12 09:30:27 -0300

    Se agregan modificaciones al dashboard de usuario
    Se agregan modificaciones a dashboard usuario

--- Frontend Web/src/app/auth/dashboard-usuario/dashboard-usuario.component.ts
+++ Frontend Web/src/app/auth/dashboard-usuario/dashboard-usuario.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router'; // Importa Router
 import { EstadisUsuariosService } from 'src/app/servicios/estadis-usuarios.service';
-import { AuthService } from 'src/app/servicios/auth.service';
-
-
 
 @Component({
   selector: 'app-dashboard-usuario',
@@ -16,23 +12,17 @@ export class DashboardUsuarioComponent implements OnInit {
   notas_glucemia: any;
   servicios: any;
   formNotasPOST: FormGroup;
-  formActualizarNota: FormGroup; // Agregar el FormGroup para el formulario de actualización
-
-  // Variables para el carrito
+  formActualizarNota: FormGroup;
+  serviciosCarrito: any[] = [];
   Snombre: string = '';
   Smonto: string = '';
   nuevoPedido: any[] = [];
+  showForm: boolean = false;
 
-  serviciosCarrito: any;
-
-/////////////////////////////////////////////////
   constructor(
     private paciente: EstadisUsuariosService,
-    private usuario: AuthService,
     private formBuilder: FormBuilder,
-    private router: Router,
-    private routerAct: ActivatedRoute,
-    private http: HttpClient
+    private router: Router
   ) {
     this.formNotasPOST = this.formBuilder.group({
       fecha_registro: ['', Validators.required],
@@ -40,67 +30,39 @@ export class DashboardUsuarioComponent implements OnInit {
       comentario_registro: ['', Validators.required]
     });
 
-    // Inicializar el FormGroup para el formulario de actualización
     this.formActualizarNota = this.formBuilder.group({
       fecha_registro: ['', Validators.required],
       valor_glucemia: ['', [Validators.required, Validators.maxLength(3)]],
       comentario_registro: ['', Validators.required]
     });
   }
-////////////////////////////////////////////////
+
   ngOnInit(): void {
     this.getNotas();
     this.getServicios();
     this.getCarrito();
-
-    // Codigo que muestra las notas (instancia)
-    this.paciente.muestraNotasUsuario().subscribe({
-      next:(notas_G)=>{
-        this.notas_glucemia=notas_G
-      },
-      error:(errorData)=>{
-        console.error(errorData);
-      } 
-    });
-
-    // Codigo que muestra los servicios (instancia)
-    this.paciente.muestraServicioAUsuario().subscribe({
-      next:(servicio)=>{
-        this.servicios=servicio
-      },
-      error:(errorData)=>{
-        console.error(errorData);
-      } 
-    })    
-
   }
-////////////////////////////////////////////////
-
-  //////////// CODIGO NOTAS ///////////////
-  //////////// CODIGO NOTAS ///////////////
-  //////////// CODIGO NOTAS ///////////////
-
-  // AGREGAR NOTA
+// Agrega el método toggleForm para mostrar/ocultar el formulario
+toggleForm(): void {
+  this.showForm = !this.showForm;
+}
   agregarNota(): void {
     if (this.formNotasPOST.valid) {
-      this.paciente.nuevaNota(
-        {
-          fecha_registro: this.formNotasPOST.value.fecha_registro,
-          valor_glucemia: this.formNotasPOST.value.valor_glucemia,
-          comentario_registro: this.formNotasPOST.value.comentario_registro
-        })
-        .subscribe((respuesta: any) => {
-          alert('Nota registrada');
-          this.getNotas();
-          this.formNotasPOST.reset();
-        });
+      this.paciente.nuevaNota({
+        fecha_registro: this.formNotasPOST.value.fecha_registro,
+        valor_glucemia: this.formNotasPOST.value.valor_glucemia,
+        comentario_registro: this.formNotasPOST.value.comentario_registro
+      }).subscribe((respuesta: any) => {
+        alert('Nota registrada');
+        this.getNotas();
+        this.formNotasPOST.reset();
+      });
     } else {
       alert('Ingrese los datos correctamente');
       this.formNotasPOST.markAllAsTouched();
     }
   }
 
-  // METODO GET NOTAS
   getNotas(): void {
     this.paciente.muestraNotasUsuario().subscribe({
       next: (notas_S) => {
@@ -112,7 +74,6 @@ export class DashboardUsuarioComponent implements OnInit {
     });
   }
 
-  // METODO DELETE NOTAS
   eliminar(id: string): void {
     this.paciente.DELETE(id).subscribe(() => {
       alert('Nota Eliminada');
@@ -121,7 +82,6 @@ export class DashboardUsuarioComponent implements OnInit {
     });
   }
 
-  // METODO PARA CARGAR DATOS EN FORMULARIO DE ACTUALIZACIÓN
   cargarNotaParaActualizar(nota: any): void {
     this.formActualizarNota.patchValue({
       fecha_registro: nota.fecha_registro,
@@ -130,7 +90,6 @@ export class DashboardUsuarioComponent implements OnInit {
     });
   }
 
-  // METODO PARA ACTUALIZAR NOTA
   actualizarNota(id: string): void {
     if (this.formActualizarNota.valid) {
       const datosActualizados = {
@@ -139,10 +98,9 @@ export class DashboardUsuarioComponent implements OnInit {
         comentario_registro: this.formActualizarNota.value.comentario_registro
       };
       
-      const idNumber: number = parseInt(id, 10); // Convertir `id` a tipo number
-this.paciente.modificar2(datosActualizados, idNumber).subscribe((respuesta: any) => {
-  alert('Nota actualizada');
-
+      const idNumber: number = parseInt(id, 10);
+      this.paciente.modificar2(datosActualizados, idNumber).subscribe((respuesta: any) => {
+        alert('Nota actualizada');
       });
     } else {
       alert('Ingrese los datos correctamente');
@@ -150,23 +108,17 @@ this.paciente.modificar2(datosActualizados, idNumber).subscribe((respuesta: any)
     }
   }
 
-  //////////////// CODIGO DEL CARRITO /////////////////
-  //////////////// CODIGO DEL CARRITO /////////////////
-  //////////////// CODIGO DEL CARRITO /////////////////
-
-  // Método para obtener los servicios en el carrito
-  getCarrito(){
-    this.paciente.muestraCarritoAUsuario().subscribe({
-      next: (servicios_C) => {
-        this.serviciosCarrito = servicios_C;
+  getCarrito(): void {
+    this.paciente.muestraCarritoAUsuario().subscribe(
+      (servicios_C: any) => {
+        this.serviciosCarrito = servicios_C as any[];
       },
-      error: (errorData) => {
+      (errorData: any) => {
         console.error(errorData);
       }
-    });
+    );
   }
 
-  // Método para agregar un servicio al carrito
   agregarCarrito(servicio: any): void {
     this.paciente.agregaAlCarrito(servicio)
       .subscribe((data) => {
@@ -175,7 +127,6 @@ this.paciente.modificar2(datosActualizados, idNumber).subscribe((respuesta: any)
       });
   }
 
-  // Método para eliminar un servicio del carrito
   eliminarServ(id:string){
     this.paciente.DELETE_SERV(id).subscribe(()=>{
       alert("Servicio eliminado del carrito")
@@ -184,7 +135,6 @@ this.paciente.modificar2(datosActualizados, idNumber).subscribe((respuesta: any)
     })
   }
 
-  // Método para obtener los servicios disponibles
   getServicios(): void {
     this.paciente.muestraServicioAUsuario().subscribe({
       next: (servicios_S) => {
@@ -195,10 +145,6 @@ this.paciente.modificar2(datosActualizados, idNumber).subscribe((respuesta: any)
       }
     });
   }
-
-  ///////////////////////////////////////////
-  ///////////////////////////////////////////
-  ///////////////////////////////////////////
 
   agregarNombre(value: string): void {
     this.Snombre = value;
