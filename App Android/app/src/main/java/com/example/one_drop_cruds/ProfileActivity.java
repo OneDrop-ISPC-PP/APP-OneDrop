5dddb6be63ea48b4df08e8d2b5c6b7f27031d004
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2024-05-28 17:55:48 -0300

    feat (modificacion ficha medica) agregado de logica para modificar datos de ficha medica

--- App Android/app/src/main/java/com/example/one_drop_cruds/ProfileActivity.java
+++ App Android/app/src/main/java/com/example/one_drop_cruds/ProfileActivity.java
@@ -1,6 +1,5 @@
 package com.example.one_drop_cruds;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,9 +12,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.example.one_drop_cruds.entities.DTOmedicalRecord;
+import com.example.one_drop_cruds.entities.dtos.medicalRecords.AddNewMedicalRecordDto;
+import com.example.one_drop_cruds.entities.dtos.medicalRecords.MedicalRecordReadDto;
 import com.example.one_drop_cruds.entities.user.FichaMedicaUsuario;
 import com.example.one_drop_cruds.entities.user.LoguedUserDetails;
 import com.example.one_drop_cruds.entities.user.enums.Terapia_insulina;
@@ -24,9 +23,11 @@ import com.example.one_drop_cruds.entities.user.enums.Tipo_diabetes;
 import com.example.one_drop_cruds.entities.user.enums.Tipo_glucometro;
 import com.example.one_drop_cruds.entities.user.enums.Tipo_sensor;
 import com.example.one_drop_cruds.request.AuthRequests;
-import com.example.one_drop_cruds.utils.AdminSQLiteOpenHelper;
+import com.example.one_drop_cruds.request.MedicalRecordRequest;
 import com.example.one_drop_cruds.utils.BackendUrl;
+import com.example.one_drop_cruds.utils.RetrofitHelper;
 import com.example.one_drop_cruds.utils.SharedPrefManager;
+import com.example.one_drop_cruds.utils.ToastHelper;
 import com.example.one_drop_cruds.utils.UserSessionManager;
 
 import java.lang.reflect.Field;
@@ -34,20 +35,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProfileActivity extends AppCompatActivity {
     UserSessionManager userSessionManager;
     SharedPrefManager sharedPrefManager;
-    // AdminSQLiteOpenHelper admin;
-    EditText signup_name, signup_last_name, signup_age, signup_birth, signup_weight, signup_db_type, signup_comorbilidades, signup_objetivo_glucosa;
+    EditText signup_name, signup_last_name, signup_birth, signup_weight, signup_comorbilidades, signup_objetivo_glucosa;
     private Spinner spinnerDiabetes, spinnerTerapiaInsulina, spinnerTerapiaPastillas, spinnerTipoGlucometro, spinnerTipoSensor;
     Button edit_medical_data_button, selectImageButton;
     ImageView profileImage;
@@ -58,6 +53,9 @@ public class ProfileActivity extends AppCompatActivity {
     FichaMedicaUsuario fichaMedicaUsuario;
     String token;
     String baseUrl = new BackendUrl().getBackendUrl();
+    AuthRequests authRequests;
+    MedicalRecordRequest medicalRecordRequest;
+    ToastHelper toastHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,9 +66,13 @@ public class ProfileActivity extends AppCompatActivity {
 
         loguedUser = userSessionManager.getLoguedUserDetails();  // SI NO ESTA LOGUEADO, SE REDIRIGE A LOGIN
 
+        // request, Crea helper con el jwt, inicializa retrofit y crea AuthRequests, para hacer solicitudes
+        authRequests = new RetrofitHelper(token).getRetrofitHelperWithToken().create(AuthRequests.class);
+        medicalRecordRequest = new RetrofitHelper(token).getRetrofitHelperWithToken().create(MedicalRecordRequest.class);
+        toastHelper = new ToastHelper(ProfileActivity.this);
+
         signup_name = findViewById(R.id.signup_name);
         signup_last_name = findViewById(R.id.signup_last_name);
-        // signup_age = findViewById(R.id.signup_age);
         signup_birth = findViewById(R.id.signup_birth);
         signup_weight = findViewById(R.id.signup_weight);
         signup_comorbilidades = findViewById(R.id.signup_comorbilidades);
@@ -86,41 +88,17 @@ public class ProfileActivity extends AppCompatActivity {
         setTextsForm();
     }
 
-    private void getFichaMedicaUsuario(){
-        HttpLoggingInterceptor getDetailsUserInterceptor = new HttpLoggingInterceptor();
-        getDetailsUserInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        httpClient.addInterceptor(getDetailsUserInterceptor);
-
-        // todo el interceptor para agregar el token al header
-        httpClient.addInterceptor(chain -> {
-            Request originalRequest = chain.request();
-            Request.Builder builder = originalRequest.newBuilder();
-            if (token!= null &&!token.isEmpty()) {
-                builder.header("Authorization", "Bearer " + token);
-            }
-            return chain.proceed(builder.build());
-        }); //todo el interceptor para agregar el token al header
-
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(httpClient.build())
-                .build();
-        AuthRequests authRequest = retrofit.create(AuthRequests.class);
-
-
-        Call<FichaMedicaUsuario> call = authRequest.getFichaMedicaUsuario(loguedUser.getId());
+    private void getFichaMedicaUsuarioRequest(){
+        Call<FichaMedicaUsuario> call = authRequests.getFichaMedicaUsuario(loguedUser.getId());
         call.enqueue(new Callback<FichaMedicaUsuario>() {
             @Override
             public void onResponse(Call<FichaMedicaUsuario> call, Response<FichaMedicaUsuario> response) {
                 if(response.isSuccessful() && response.body() != null){
                     // Obtener datos de ficha medica y guardarlo en shared
-                    System.out.println("******************** FICHA MEDICA *************************************************");
+                    System.out.println("******************** FICHA MEDICA ********************");
                     System.out.println(response.body());
                     setFichaMedica(response.body());
-                    System.out.println("******************** FICHA MEDICA ******************************************************");
+                    System.out.println("******************** FICHA MEDICA ********************");
                 } else if (response.code()==400){
                     System.out.println(" FICHA MEDICA response.code()==400 SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA *********");
                     System.out.println(response.body());
@@ -151,7 +129,7 @@ public class ProfileActivity extends AppCompatActivity {
         Field[] opcionesEnum = enumerable.getDeclaredFields();
         for (Field field : opcionesEnum) {
             if(field.getName() != "$VALUES"){
-                opciones.add(field.getName()); // si funciona cambiar en ProfileActivity.java
+                opciones.add(field.getName().replace("_", " ")); // si funciona cambiar en ProfileActivity.java
             }
         }
 
@@ -163,7 +141,7 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 // si la opcion es distinta a la original..
-                if (! spinner.getSelectedItem().toString().equals(elementoPreseleccionado)){
+                if (! spinner.getSelectedItem().toString().replace("_", " ").equals(elementoPreseleccionado)){
                     // Aquí puedes manejar la selección del usuario
                     System.out.println("USUARIO CAMBIO OPCION A "+spinner.getSelectedItem().toString());
                 }
@@ -181,14 +159,14 @@ public class ProfileActivity extends AppCompatActivity {
         signup_comorbilidades.setText(ficha.getComorbilidades());
 
         //Sets listas desplegables
-        setSpinner(ficha, Tipo_diabetes.class, spinnerDiabetes ,  ficha.getTipo_diabetes().name());
-        setSpinner(ficha, Terapia_insulina.class, spinnerTerapiaInsulina ,  ficha.getTerapia_insulina().name());
-        setSpinner(ficha, Terapia_pastillas.class, spinnerTerapiaPastillas ,  ficha.getTerapia_pastillas().name());
-        setSpinner(ficha, Tipo_glucometro.class, spinnerTipoGlucometro ,  ficha.getTipo_glucometro().name());
-        setSpinner(ficha, Tipo_sensor.class, spinnerTipoSensor ,  ficha.getTipo_sensor().name());
+        setSpinner(ficha, Tipo_diabetes.class, spinnerDiabetes ,  ficha.getTipo_diabetes().name().replace("_", " "));
+        setSpinner(ficha, Terapia_insulina.class, spinnerTerapiaInsulina ,  ficha.getTerapia_insulina().name().replace("_", " "));
+        setSpinner(ficha, Terapia_pastillas.class, spinnerTerapiaPastillas ,  ficha.getTerapia_pastillas().name().replace("_", " "));
+        setSpinner(ficha, Tipo_glucometro.class, spinnerTipoGlucometro ,  ficha.getTipo_glucometro().name().replace("_", " "));
+        setSpinner(ficha, Tipo_sensor.class, spinnerTipoSensor ,  ficha.getTipo_sensor().name().replace("_", " "));
     }
     public void setTextsForm() {
-        getFichaMedicaUsuario(); // Carga datos en fichaMedicaUsuario, o inicia activity para cargar datos de ficha medica
+        getFichaMedicaUsuarioRequest(); // Carga datos en fichaMedicaUsuario, o inicia activity para cargar datos de ficha medica
 
         // DTOmedicalRecord medicalRecord = // admin.getMedicalRecord(loguedUser.getUsername());
         signup_name.setText(loguedUser.getNombre());
@@ -197,8 +175,107 @@ public class ProfileActivity extends AppCompatActivity {
         List<Integer> nac = loguedUser.getNacimiento();
         signup_birth.setText(nac.get(2)+"/"+nac.get(1)+"/"+nac.get(0));
     }
+    private boolean changesInUserProfile(){
+        String nombre = signup_name.getText().toString();
+        Boolean sinCambioNombre = loguedUser.getNombre().equals(nombre);
+        String apellido = signup_last_name.getText().toString();
+        Boolean sinCambioApellido = loguedUser.getApellido().equals(apellido);
+        String nacimiento = signup_birth.getText().toString();
+        Boolean sinCambioNacimiento = loguedUser.getNacimiento().equals(nacimiento);
+        if (sinCambioNombre && sinCambioApellido && sinCambioNacimiento){
+            return false;
+        }
+        return true;
+    }
+    private boolean changesInMedicalRecords(FichaMedicaUsuario fichaGuardada){
+        Double peso = Double.valueOf(signup_weight.getText().toString());
+        Boolean sinCambioPeso = fichaGuardada.getPeso().equals(peso);
 
+        String comorbilidad = signup_comorbilidades.getText().toString();
+        Boolean sinCambioComorbilidad = fichaGuardada.getComorbilidades().equals(comorbilidad);
+
+        String glucosa = signup_objetivo_glucosa.getText().toString();
+        Boolean sinCambioGlucosa = fichaGuardada.getObjetivo_glucosa().equals(glucosa);
+
+        String diabetes = spinnerDiabetes.getSelectedItem().toString();
+        Boolean sinCambioDiabetes = fichaGuardada.getTipo_diabetes().toString().replace("_", " ").equals(diabetes);
+
+        String insulina = spinnerTerapiaInsulina.getSelectedItem().toString();
+        Boolean sinCambioInsulina= fichaGuardada.getTerapia_insulina().toString().replace("_", " ").equals(insulina);
+
+        String pastillas = spinnerTerapiaPastillas.getSelectedItem().toString();
+        Boolean sinCambioPastillas = fichaGuardada.getTerapia_pastillas().toString().replace("_", " ").equals(pastillas);
+
+        String glucometro = spinnerTipoGlucometro.getSelectedItem().toString();
+        Boolean sinCambioGlucometro= fichaGuardada.getTipo_glucometro().toString().replace("_", " ").equals(glucometro);
+
+        String sensor = spinnerTipoSensor.getSelectedItem().toString();
+        Boolean sinCambioSensor = fichaGuardada.getTipo_sensor().toString().replace("_", " ").equals(sensor);
+
+        if (sinCambioPeso && sinCambioComorbilidad && sinCambioGlucosa && sinCambioDiabetes && sinCambioInsulina &&sinCambioPastillas && sinCambioGlucometro && sinCambioSensor ){
+            return false;
+        }
+        return true;
+    }
+
+    private void updateUserProfile(){
+        System.out.println(">>>>>>>>>>>>>>>>> CAMBIOS EN USUARIO >>>>>");
+        // todo  si hay en perfil, le pego a endpoint de edit user, nombre, apellido, fecha nacimiento
+
+    }
+    private void updateMedicalRecord(){
+        Double peso = Double.valueOf(signup_weight.getText().toString());
+        String comorbilidad = signup_comorbilidades.getText().toString();
+        String glucosa = signup_objetivo_glucosa.getText().toString();
+        String diabetes = spinnerDiabetes.getSelectedItem().toString().replace(" ", "_");
+        String insulina = spinnerTerapiaInsulina.getSelectedItem().toString().replace(" ", "_");
+        String pastillas = spinnerTerapiaPastillas.getSelectedItem().toString().replace(" ", "_");
+        String glucometro = spinnerTipoGlucometro.getSelectedItem().toString().replace(" ", "_");
+        String sensor = spinnerTipoSensor.getSelectedItem().toString().replace(" ", "_");
+
+        AddNewMedicalRecordDto updateDto = new AddNewMedicalRecordDto(null, diabetes, insulina, pastillas, glucometro, sensor, glucosa, comorbilidad, peso);
+
+        Call<MedicalRecordReadDto> call = medicalRecordRequest.editMedicalRecord(loguedUser.getId(), updateDto);
+        call.enqueue(new Callback<MedicalRecordReadDto>() {
+            @Override
+            public void onResponse(Call<MedicalRecordReadDto> call, Response<MedicalRecordReadDto> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    // Obtener datos de ficha medica y guardarlo en shared
+                    System.out.println("******************** updateMedicalRecord ********************");
+                    System.out.println(response.body());
+                    toastHelper.showLong("Datos modificados!");
+                    System.out.println("******************** updateMedicalRecord ********************");
+                } else if (response.code()==400){
+                    System.out.println(" updateMedicalRecord response.code()==400 SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA *********");
+                    toastHelper.showLong("Error modificando datos!");
+                    System.out.println(response.body());
+                    // TODO SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA
+                    // TODO SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA
+                    // TODO SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA
+                    System.out.println(" FICHA MEDICA response.code()==400 SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA *********");
+                }
+            }
+            @Override
+            public void onFailure(Call<MedicalRecordReadDto> call, Throwable t) {
+                System.out.println("******************** updateMedicalRecord Throwable t*************************************************");
+                System.out.println( t);
+                toastHelper.showLong("Error modificando datos: "+t.getMessage());
+                // TODO SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA
+                // TODO SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA
+                // TODO SI NO ESTA CARGADA LA FICHA, SE DEBERIA REDIRIGIR A ACTIVIY DE CARGA DE FICHA MEDICA
+                System.out.println("******************** FICHA MEDICA Throwable t******************************************************");
+            }
+        });
+    }
     public void updateMedicalRecord(View v) {
+        FichaMedicaUsuario fichaGuardada = sharedPrefManager.getFichaMedicaUser();
+        if(changesInMedicalRecords(fichaGuardada)){
+            updateMedicalRecord();
+        }
+        if(changesInUserProfile()){
+            updateUserProfile();
+        }
+
         /*
         String name = signup_name.getText().toString();
         String last_name = signup_last_name.getText().toString();
@@ -224,7 +301,6 @@ public class ProfileActivity extends AppCompatActivity {
                     Toast.makeText(this, "Error al guardar la URI de imagen", Toast.LENGTH_SHORT).show();
                 }
             }
-
             boolean insertResult = admin.updateMedicalRecord(updateDtoMedical);
             if (insertResult) {
                 Toast.makeText(this, "Datos modificados exitosamente", Toast.LENGTH_SHORT).show();
