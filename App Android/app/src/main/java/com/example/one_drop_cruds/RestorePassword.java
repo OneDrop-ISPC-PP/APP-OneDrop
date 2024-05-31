e0473cc3eebb6d1fb3acc9042a8b0179845c01c3
Daniel Herrera <74433631+DHerrera85@users.noreply.github.com>
2024-05-31 19:31:58 -0300

    Se agregan cambios en User Login, Restore Password y AuthRequest

--- App Android/app/src/main/java/com/example/one_drop_cruds/RestorePassword.java
+++ App Android/app/src/main/java/com/example/one_drop_cruds/RestorePassword.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,36 +15,72 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.one_drop_cruds.entities.user.AuthResponse;
+import com.example.one_drop_cruds.entities.user.LoginRequest;
+import com.example.one_drop_cruds.request.AuthRequests;
+import com.example.one_drop_cruds.utils.BackendUrl;
 import com.example.one_drop_cruds.utils.ToastHelper;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RestorePassword extends AppCompatActivity implements View.OnClickListener {
 
     EditText restore_password_email;
     Button restore_password_btn;
     ToastHelper toastHelper;
+    String backendUrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restore_password);
 
-
         restore_password_email = findViewById(R.id.restore_password_email);
         restore_password_btn = findViewById(R.id.restore_password_btn);
-/*
-        restore_password_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                restorePasswordRequest();
-            }
-        });
-*/
 
         toastHelper= new ToastHelper(RestorePassword.this);
+        backendUrl = new BackendUrl().getBackendUrl();
 
     }
 
     public void restorePasswordRequest(View v){
-        toastHelper.showLong("SE ENVIA SOLICITUD PARA RESTAURAR PASSWORD");
+        String email = restore_password_email.getText().toString();
+        // logica de request
+        HttpLoggingInterceptor logginInterceptor = new HttpLoggingInterceptor();
+        logginInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logginInterceptor);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(backendUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+        AuthRequests authRequest = retrofit.create(AuthRequests.class);
+
+        Call<Boolean> call = authRequest.getRestorePassword(email);
+        call.enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                // responde true o false
+                if(response.isSuccessful() && response.body() != null && response.body()){
+                    Intent intent = new Intent(getApplicationContext(), UserLoginActivity.class);
+                    startActivity(intent);
+                    toastHelper.showLong("Se enviaron mas instrucciones al email!");
+                } else if(! response.isSuccessful()){
+                    toastHelper.showLong("Error enviando email, revisa el email!");
+                }
+            }
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+                toastHelper.showLong("Error : "+t.getMessage());
+            }
+        });
     }
 
     public void toLogin(View v){
